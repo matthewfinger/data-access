1b3e5ade4f946b506de492a97f4184b39edd3288
Matt Finger <matthewfinger3@gmail.com>
2023-01-08 13:52:54 -0600

    Executed a select query

--- main.go
+++ main.go
@@ -8,9 +8,38 @@ import (
 	"log"
 )
 
-
 var db *sql.DB
 
+type Album struct {
+    ID     int64
+    Title  string
+    Artist string
+    Price  float32
+}
+
+func albumsByArtist(name string) ([]Album, error) {
+	var albums []Album
+	rows, err := db.Query("SELECT * FROM album WHERE artist = ?;", name)
+	if err != nil {
+		return nil, fmt.Errorf("abumsByArtist %q: %v", name, err)
+	}
+
+	defer rows.Close()
+	// put rows in the output array
+	for rows.Next() {
+		var album Album
+		if err := rows.Scan(&album.ID, &album.Title, &album.Artist, &album.Price); err != nil {
+			return nil, fmt.Errorf("albumsByArtist %q: %v", name, err)
+		}
+		albums = append(albums, album)
+	}
+
+	if err := rows.Err(); err != nil {
+		return nil, fmt.Errorf("albumsByArtist %q: %v", name, err)
+	}
+
+	return albums, nil
+}
 
 func main() {
 	var err error
@@ -48,4 +77,10 @@ func main() {
 	}
 
 	fmt.Println("Connected!")
+
+	albums, err := albumsByArtist("John Coltrane")
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Printf("Albums found: %v\n", albums)
 }
